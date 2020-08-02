146f02c446a5b05de91bc41f2faabd4b4effe99b
daniel.galiano <daniel.galiano@bbvaitspain.com>
2020-08-02 20:45:59 +0200

    removerd 2

--- TestDanielo/src/com/dani/test/PruebaCambios.java
+++ TestDanielo/src/com/dani/test/PruebaCambios.java
@@ -9,14 +9,14 @@ public class PruebaCambios {
 		//class for testing
 		String zero = "0";
 		String uno = "1";
-		String dos = "2";
+		
 		String tres = "3";
 		String cuatro = "4";
 		String cinco = "5";
 		String seis = "6";
 		
 		System.out.println(uno);
-		System.out.println(dos);
+		
 		System.out.println(tres);
 		System.out.println(cuatro);
 		System.out.println(cinco);
