ace45302f5dfcca661be18dca5b4fa7f11478048
daniel.galiano <daniel.galiano@bbvaitspain.com>
2020-08-02 20:49:54 +0200

    cambio ocho

--- TestDanielo/src/com/dani/test/PruebaCambios.java
+++ TestDanielo/src/com/dani/test/PruebaCambios.java
@@ -9,20 +9,20 @@ public class PruebaCambios {
 		//class for testing
 		String zero = "0";
 		String uno = "1";
-		
 		String tres = "3";
 		String cuatro = "4";
 		String cinco = "5";
 		String seis = "6";
 		String siete = "7";
+		String ocho = "8";
 		
 		System.out.println(uno);
-		
 		System.out.println(tres);
 		System.out.println(cuatro);
 		System.out.println(cinco);
 		System.out.println(seis);
 		System.out.println(siete);
+		System.out.println(ocho);
 
 	}
 
